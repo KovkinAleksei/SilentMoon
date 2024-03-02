243067cd4841bb7842b4aa6006e0092b3fceda32
KovkinAleksei <akovkin6@gmail.com>
2024-03-02 16:32:14 +0700

    Добавлена разметка экрана music v2

--- app/src/main/java/com/example/mobile_hard_mad_lab1/musicScreen/MusicFragment.kt
+++ app/src/main/java/com/example/mobile_hard_mad_lab1/musicScreen/MusicFragment.kt
@@ -1,10 +1,15 @@
 package com.example.mobile_hard_mad_lab1.musicScreen
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
+import com.example.mobile_hard_mad_lab1.common.MarginFix
 import com.example.mobile_hard_mad_lab1.databinding.FragmentMusicBinding
 
 
@@ -16,12 +21,38 @@ class MusicFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMusicBinding.inflate(inflater, container, false)
+        fixMargins(container)
+
+        val navController = findNavController()
+        val cancelButton = binding.cancelButton
+
+        cancelButton.setOnClickListener{
+            navController.navigateUp()
+        }
 
         return binding.root
+    }
+
+    // Выравнивание отступов относительно статус бара и системной навигационной панели
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun fixMargins(container: ViewGroup?) {
+        container?.setOnApplyWindowInsetsListener{v, insets ->
+            val systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val cancelButton = binding.cancelButton
+
+            if (!marginIsFixed) {
+                MarginFix.addTopMargin(cancelButton, systemInsets)
+
+                marginIsFixed = true
+            }
+
+            insets
+        }
     }
 }
